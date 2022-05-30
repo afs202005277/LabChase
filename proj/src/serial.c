0d442223ce36d6d59b40eaf973b7f676c6664f1d
Andre Sousa <up202005277@up.pt>
2022-05-30 10:17:28 +0100

    It compiles

--- proj/src/serial.c
+++ proj/src/serial.c
@@ -1,34 +1,45 @@
-#include <lcom/lcf.h>
 #include "serial.h"
+#include <lcom/lcf.h>
 
 int serial_hook = COM1_IRQ;
 
-int write_to_IER(uint8_t byte){
+uint8_t receivedChar = 0;
+
+int clear_DLAB(){
   uint8_t wordInLCR;
   read_from_LCR(&wordInLCR);
   write_to_LCR(wordInLCR & 0x7F); // set DLAB = 0
-  return sys_outb(COM1_PORT + IER, byte);;
+  return 0;
 }
 
-int write_to_LCR(uint8_t byte){
+int write_to_IER(uint8_t byte) {
+  clear_DLAB();
+  return sys_outb(COM1_PORT + IER, byte);
+}
+
+int write_to_LCR(uint8_t byte) {
   return sys_outb(COM1_PORT + LCR, byte);
 }
 
-int read_from_LCR(uint8_t* byte){
+int read_from_LCR(uint8_t *byte) {
   return util_sys_inb(COM1_PORT + LCR, byte);
 }
 
-int write_to_FCR(uint8_t byte){
+int write_to_FCR(uint8_t byte) {
   return sys_outb(COM1_PORT + FCR, byte);
 }
 
-int serial_subscribe(uint8_t* bit_no) {
+int write_to_THR(uint8_t byte){
+  return sys_outb(COM1_PORT+THR, byte);
+}
+
+int serial_subscribe(uint8_t *bit_no) {
   uint8_t enableInterrupts = 1; // enables received data available interrupt
   uint8_t byteInLCR;
   write_to_IER(enableInterrupts);
   read_from_LCR(&byteInLCR);
   write_to_LCR(byteInLCR | BIT(1) | BIT(0)); // set word of 8 bits
-  write_to_FCR(BIT(2) | BIT(1) | BIT(0)); // clears the fifos and enables them
+  write_to_FCR(BIT(2) | BIT(1) | BIT(0));    // clears the fifos and enables them
 
   *bit_no = serial_hook;
   sys_irqsetpolicy(COM1_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &serial_hook);
@@ -40,3 +51,50 @@ int serial_unsubscribe() {
     return -1;
   return 0;
 }
+
+int read_LSR(uint8_t *byte) {
+  return util_sys_inb(COM1_PORT + LSR, byte);
+}
+
+int read_RBR(uint8_t *byte){
+  clear_DLAB();
+  return util_sys_inb(COM1_PORT + RBR, byte);
+}
+
+int read_IIR(uint8_t* byte){
+  return util_sys_inb(COM1_PORT + IIR, byte);
+}
+
+int send_character(uint8_t character) {
+  uint8_t numTimesWaited = 0;
+  uint8_t lsr;
+  while (numTimesWaited < MAX_WAIT) {
+      read_LSR(&lsr);
+      if ((lsr & CAN_WRITE_TO_THR) != 0){
+        return write_to_THR(character);
+      }
+      tickdelay(micros_to_ticks(SERIAL_DELAY));
+      numTimesWaited++;
+  }
+  printf("Waited too many times to send character.\n");
+  return 1;
+}
+
+int read_character(uint8_t* character){
+  uint8_t status;
+  do{
+    read_LSR(&status);
+    if ((status & DATA_AVAILABLE) != 0){
+      read_RBR(character);
+    }
+  } while((status & DATA_AVAILABLE) != 0);
+  return 0;
+}
+
+void serial_ih(){
+  uint8_t tmp;
+  read_character(&tmp);
+  receivedChar = tmp;
+}
+
+
--- proj/src/serial.h
+++ proj/src/serial.h
@@ -1,15 +1,55 @@
 #ifndef __SERIAL_H
 #define __SERIAL_H
+#include <stdint.h>
 
 #define COM1_PORT 0x3F8
 #define COM1_IRQ 4
 #define IER 1
 #define LCR 3
 #define FCR 2
+#define LSR 5
+#define THR 0
+#define RBR 0
+#define IIR 2
+#define MAX_WAIT 5
+#define SERIAL_DELAY 2000
+
+#define CAN_WRITE_TO_THR BIT(5)
+#define DATA_AVAILABLE BIT(0)
 
 
 int serial_subscribe(uint8_t* bit_no);
 
 int serial_unsubscribe();
+
+int send_character(uint8_t character);
+
+int read_character(uint8_t* character);
+
+void serial_ih();
+
+int clear_DLAB();
+
+int write_to_IER(uint8_t byte);
+
+int write_to_LCR(uint8_t byte);
+
+int read_from_LCR(uint8_t *byte);
+
+int write_to_FCR(uint8_t byte);
+
+int write_to_THR(uint8_t byte);
+
+int read_LSR(uint8_t *byte);
+
+int read_RBR(uint8_t *byte);
+
+int read_IIR(uint8_t* byte);
+
+int send_character(uint8_t character);
+
+int read_character(uint8_t* character);
+
+void serial_ih();
 
 #endif
